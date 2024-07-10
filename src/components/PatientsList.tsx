5c927a76cd043df9ea235dbccea34c9b6323f5d0
lemus <lemus777@gmail.com>
2024-07-10 16:03:10 +0200

    writing the state in Zustand

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,5 +1,8 @@
+import { usePatientStore } from "../store"
 
 export default function PatientsList() {
+  const patients = usePatientStore(state => state.patients)
+  console.log(patients)
   return (
     <div>PatientsList</div>
   )
