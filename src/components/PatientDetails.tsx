49947535ddd137c770bbfe6e082ec58d8ac5ab4f
lemus <lemus777@gmail.com>
2024-07-10 16:38:52 +0200

    listing patients

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,12 @@
+import { Patient } from "../types"
+
+type PatientDetailsProps = {
+  patient: Patient
+}
+export default function PatientDetails({patient} : PatientDetailsProps) {
+  return (
+    <div>
+      {patient.name}
+    </div>
+  )
+}
